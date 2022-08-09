67a1f940da8e3641d4d94a40e91a0f4feaa45a23
Lucas <lucasgnclvs1@gmail.com>
2022-08-09 20:51:14 -0300

    Calculo de retangulos

---
+++ Exercicio/Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp3
+{
+    internal class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double ValorPerimetroRetangulo()
+        {
+            return (Altura * 2) + (Largura * 2);
+        }
+        public double ValorDiagonalRetangulo()
+        {
+            return (Math.Sqrt(Altura * Altura + Largura * Largura));
+        }
+        public double ValorAreaRetangulo()
+        {
+            return Altura * Largura;
+        }
+
+    }
+
+
+}
---
+++ Exercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+    namespace ConsoleApp3
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Retangulo r;
+                r = new Retangulo();
+
+                Console.WriteLine("Entre com a altura do retângulo: ");
+                r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("Entre com a largura do retângulo: ");
+                r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("AREA: " + r.ValorAreaRetangulo().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("PERIMETRO: " + r.ValorPerimetroRetangulo().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("DIAGONAL: " + r.ValorDiagonalRetangulo().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }    
+    }
